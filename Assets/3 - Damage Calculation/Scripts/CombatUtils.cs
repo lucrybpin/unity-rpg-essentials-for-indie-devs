60ae5b5a0cbfbac61716ac9a9c2684f9c2069048
lucspinto <lucspinto@outlook.com>
2025-10-17 16:40:09 -0300

    Lesson 20 - Damage Calculation - Core

---
+++ Assets/3 - Damage Calculation/Scripts/CombatUtils.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public static class CombatUtils
+{
+    static int CalculateDamage(int baseDamage, int attributePoints, bool isCritical)
+    {
+        float multiplier = 1 + (attributePoints / 10f); // 1 point = +1% damage
+        float damage = Mathf.FloorToInt(baseDamage * multiplier);
+
+        if (isCritical)
+            damage *= 2;
+
+        return (int)damage;
+    }
+}
