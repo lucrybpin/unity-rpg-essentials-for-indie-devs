463104dbf27d0dc53f91394562ccf0e33603260d
lucspinto <lucspinto@outlook.com>
2025-10-23 12:28:28 -0300

    Lesson 36 - Inventory - Add Item

--- Assets/5 - Inventory/Scripts/Inventory.cs
+++ Assets/5 - Inventory/Scripts/Inventory.cs
@@ -1,13 +1,61 @@
 using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 [Serializable]
 public class Inventory
 {
-    // List of Slots
+    [field: SerializeField] public List<InventorySlot> Slots { get; private set; }
 
-    // Add Item
+    public Inventory(int capacity) 
+    {
+        Slots = new List<InventorySlot>(capacity);
+        for (int i = 0; i < capacity; i++)
+            Slots.Add(new InventorySlot());
+    }
+
+    public void AddItem(Item item)
+    {
+        InventorySlot slot = FindAvilableSlotWithItem(item);
+
+        // Add to stack case
+        if (slot != null && !slot.IsFull())
+        {
+            slot.Quantity++;
+            return;
+        }
+
+        // No stack case
+        slot = FindEmptySlot();
+        if (slot == null)
+            return; // Full Inventory Case
+
+        slot.Item = item;
+        slot.Quantity = 1;
+        return;
+    }
 
     // Remove Item
 
     // Swap
+
+    InventorySlot FindAvilableSlotWithItem(Item item)
+    {
+        foreach (InventorySlot slot in Slots)
+            if (slot.Item == item && !slot.IsFull())
+                return slot;
+
+        return null;
+    }
+
+    InventorySlot FindEmptySlot()
+    {
+        foreach (InventorySlot slot in Slots)
+            if (slot.IsEmpty())
+                return slot;
+
+        return null;
+    }
+
+
 }
